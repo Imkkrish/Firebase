ba32deb0f2f2618b68ec311ff31323acce0f29b6
Krish Kumar <122594525+Imkkrish@users.noreply.github.com>
2024-07-27 09:59:35 +0530

    Update script.js

--- public/SoC/script.js
+++ public/SoC/script.js
@@ -1,49 +1,56 @@
 const rocket = document.querySelector('#rocket')
-const sky = document.querySelector('#sky')
-const ex = document.querySelector('#exhaust')
-var bottom = 0
-var last_y = 0
-var wheel;
+    const sky = document.querySelector('#sky')
+    const ex = document.querySelector('#exhaust')
+    const content = document.querySelector('.content')
+    var bottom = 0
+    var last_y = 0
+    var wheel
 
-window.addEventListener('wheel', function(e){
-  wheel = e.deltaY
-})
+    window.addEventListener('wheel', function(e){
+      wheel = e.deltaY
+    })
 
-window.addEventListener('scroll', function(e){
-  var h = window.innerHeight
-  var y = document.documentElement.scrollTop
-  var doc = document.body.offsetHeight -250
-  var perc = y / (doc - h)
-  // console.log(perc)
+    window.addEventListener('scroll', function(e){
+      var h = window.innerHeight
+      var y = document.documentElement.scrollTop
+      var doc = document.body.offsetHeight - 250
+      var perc = y / (doc - h)
 
-  if(perc < 1){
-    sky.style.bottom = -1*(perc)*100 + '%'    
-  }  
+      if (perc < 1) {
+        sky.style.bottom = -1 * (perc) * 100 + '%'    
+      }  
 
-  if(perc > 0) {
-    rocket.classList.add('shake_rocket')
-    ex.classList.add('exhaust')
-  } else {
-    rocket.classList.remove('shake_rocket')
-    ex.classList.remove('exhaust')
-  }
-  
-  if(perc > .37) {
-    ex.classList.remove('exhaust')
-  }
+      if (perc > 0) {
+        rocket.classList.add('shake_rocket')
+        ex.classList.add('exhaust')
+      } else {
+        rocket.classList.remove('shake_rocket')
+        ex.classList.remove('exhaust')
+      }
 
-  if(perc > .25) {
-    bottom = (perc - .25)*133
-  }
-  
-  if(perc > 0) {
-    bottom = (perc - .25)*133
-    if(perc - .25 < 0) {
-      bottom = 0
-    }
-  }
-  rocket.style.bottom = bottom + '%'
+      if (perc > .37) {
+        ex.classList.remove('exhaust')
+      }
 
-  last_y = y
-})
+      if (perc > .25) {
+        bottom = (perc - .25) * 133
+      }
 
+      if (perc > 0) {
+        bottom = (perc - .25) * 133
+        if (perc - .25 < 0) {
+          bottom = 0
+        }
+      }
+      rocket.style.bottom = bottom + '%'
+
+      last_y = y
+
+      // Automatic scroll to main content
+      if (y < 100) {
+        window.scrollTo({
+          top: content.offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    })
